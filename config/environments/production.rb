aeee8e2682fda924a61775e1bc3fd13ff59f6ade
Scott Shillcock <scott@shillcock.net>
2014-10-02 22:53:39 -0700

    Configure mail settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,4 @@
+require Rails.root.join("config/smtp")
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -43,7 +44,7 @@ Rails.application.configure do
   # config.force_ssl = true
 
   # Set to :info to decrease the log volume.
-  config.log_level = :debug
+  config.log_level = :info
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
@@ -60,6 +61,8 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = SMTP_SETTINGS
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -73,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'family.shillcock.net' }
 end
+
