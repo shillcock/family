2e91886c4abb7700ccd00f4f250cb332aeef3ab1
Scott Shillcock <scott@shillcock.net>
2014-10-03 22:38:13 -0700

    Automount webconsole

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,4 +40,6 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   config.action_mailer.default_url_options = { host: 'localhost:5000' }
+
+  config.web_console.automount = true
 end
