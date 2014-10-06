9ebdb0de7384eb2731324b987c1825dbe154083c
Scott Shillcock <scott@shillcock.net>
2014-10-05 20:20:30 -0700

    use postgresql for database

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,16 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port: "587",
+    authentication: :plain,
+    enable_starttls_auto: true,
+    address: ENV["SMTP_ADDRESS"],
+    domain: ENV["SMTP_DOMAIN"],
+    password: ENV["SMTP_PASSWORD"],
+    user_name: ENV["SMTP_USERNAME"]
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -74,4 +84,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: “family.shillcock.net” }
 end
