7ab932089909666d148132dffd1fc1db8e0dac00
Scott Shillcock <scott@shillcock.net>
2014-11-03 17:13:41 -0800

    Had a pending migration

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -9,9 +9,11 @@
 #  created_at     :datetime         not null
 #  updated_at     :datetime         not null
 #  user_id        :integer          not null
+#  deleted_at     :datetime
 #
 # Indexes
 #
+#  index_photos_on_deleted_at                       (deleted_at)
 #  index_photos_on_photoable_id_and_photoable_type  (photoable_id,photoable_type)
 #  index_photos_on_user_id                          (user_id)
 #
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141104000946) do
+ActiveRecord::Schema.define(version: 20141104004052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,8 +51,10 @@ ActiveRecord::Schema.define(version: 20141104000946) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.integer  "user_id",        null: false
+    t.datetime "deleted_at"
   end
 
+  add_index "photos", ["deleted_at"], name: "index_photos_on_deleted_at", using: :btree
   add_index "photos", ["photoable_id", "photoable_type"], name: "index_photos_on_photoable_id_and_photoable_type", using: :btree
   add_index "photos", ["user_id"], name: "index_photos_on_user_id", using: :btree
 
