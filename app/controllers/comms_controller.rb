520cebdbe9ded96c4fdebe4b444739fb530f92f1
Scott Shillcock <scott@shillcock.net>
2015-01-08 21:58:23 -0800

    Update sms endpoint to not care about sms_confirmed? anymore

--- app/controllers/comms_controller.rb
+++ app/controllers/comms_controller.rb
@@ -14,44 +14,20 @@ class CommsController < ApplicationController
   end
 
   def sms
-    if @user.sms_confirmed?
-      create_post_from_sms
-    elsif confirm_user
-      send_welcome_message
+    return head :not_found unless @user
+
+    post = SmsPost.new(@user, params)
+    if post.save
+      post.send_notifications!
+      head :ok
+    else
+      head :bad_request
     end
  end
 
-  private
+ private
     def set_user
-      unless @user = User.find_by(phone_number: params["From"])
-        head :not_found
-      end
-    end
-
-    def confirm_user
-      if @user.sms_token && params["Body"].include?(@user.sms_token)
-        @user.update_attributes(sms_confirmed_at: Time.zone.now, sms_token: nil)
-      else
-        false
-      end
-    end
-
-    def create_post_from_sms
-      post = SmsPost.new(@user, params)
-      if post.save
-        post.send_notifications!
-        head :ok
-      else
-        head :bad_request
-      end
-    end
-
-    def send_welcome_message
-      response = Twilio::TwiML::Response.new do |r|
-        r.Message "Thanks #{@user.first_name}, welcome to the family :)"
-      end
-
-      render_twiml response
+      @user = User.find_by(phone_number: params["From"])
     end
 
     def set_header
